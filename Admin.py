0ba957e734c70b7280fee33b483f71cf5972caf6
rosspkm <rosspkm@gmail.com>
2021-01-03 19:06:29 -0500

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -1,13 +1,114 @@
-def NewAdminAccount():
-    code = input("Please enter a 6 digit code for your new admin login: ")
+import random
+import __main__
+import string
+import json
+from lib import fdb
+
+
+def rnd():
+    return''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k=8))
+
+
+def NewAdminAccount(name):
+    password = input("Please enter a login key: ")
     first_name = input("Please enter your first name: ")
     last_name = input("please enter your last name: ")
-    with open("login.txt", "w") as FILE:
-        FILE.write(f"{code}:{first_name} {last_name}: A")
+    email = input("please enter your email: ")
+    phone_number = input("please enter your phone number(ex: xxxxxxxxxx): ")
+    db = fdb.database()
+    db.SelectDB(name)
+    db.SelectTable("users")
+    db.Insert({
+        "user-account_id": rnd(),
+        "firstname": first_name,
+        "lastname": last_name,
+        "email": email,
+        "phone-number": phone_number,
+        "account-type": "A",
+        "password": password
+    })
     print("Admin Account successfully created")
 
 
 def CreateNewUser():
+    password = input("Please enter a login key: ")
+    first_name = input("Please enter your first name: ")
+    last_name = input("please enter your last name: ")
+    email = input("please enter your email: ")
+    phone_number = input("please enter your phone number(ex: xxxxxxxxxx): ")
+    db = fdb.database()
+    db.SelectDB(__main__.restaurant.get_restaurant_name())
+    db.SelectTable("users")
+    db.Insert({
+        "user-account_id": rnd(),
+        "firstname": first_name,
+        "lastname": last_name,
+        "email": email,
+        "phone-number": phone_number,
+        "account-type": "U",
+        "password": password
+    })
+
+
+def ChangeUserType():
+    db = fdb.database()
+    db.SelectDB(__main__.restaurant.get_restaurant_name())
+    db.SelectTable("users")
+    password = input("Please enter a login key: ")
+
+    account = db.Get({
+        "password": password
+    })
+    account = json.loads(account)
+
+    print("What kind of account are you changing this to?\n[1] Admin\n[2] Manager \n[3] Employee ")
+    x = input("Enter your choice [1-3]: ")
+
+    while True:
+        if x == 1:
+            account_type = "A"
+            break
+        elif x == 2:
+            account_type = "M"
+            break
+        elif x == 3:
+            account_type = "U"
+            break
+        else:
+            print("Invalid choice")
+
+    db.Delete({
+        "password": password
+    })
+
+    first_name = account['firstname']
+    last_name = account['lastname']
+    email = account['email']
+    phone_number = account['phone-number']
+
+    db.Insert({
+        "user-account_id": rnd(),
+        "firstname": first_name,
+        "lastname": last_name,
+        "email": email,
+        "phone-number": phone_number,
+        "account-type": account_type,
+        "password": password
+    })
+
+
+def DeleteUser():
+    db = fdb.database()
+    db.SelectDB(__main__.restaurant.get_restaurant_name())
+    db.SelectTable("users")
+    password = input("Please enter a login key: ")
+
+    db.Delete({
+        "password": password
+    })
+
+
+def ShowUsers():
     print("test")
 
 
